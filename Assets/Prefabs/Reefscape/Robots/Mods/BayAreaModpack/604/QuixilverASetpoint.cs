13a7730f99b9660e1f18502dbc4497e707febfce
alonrabinowitz <62042858+alonrabinowitz@users.noreply.github.com>
2026-04-27 21:25:38 -0700

    604B initial setup and import + hierarchy

---
+++ Assets/Prefabs/Reefscape/Robots/Mods/BayAreaModpack/604/QuixilverASetpoint.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Prefabs.Reefscape.Robots.Mods.BayAreaModpack._581
+{
+    [CreateAssetMenu(fileName = "Setpoint", menuName = "Robot/Blazing Bulldogs A Setpoint", order = 0)]
+    public class BlazingBulldogsASetpoint : ScriptableObject
+    {
+        [Tooltip("Inches")] public float elevatorHeight;
+        [Tooltip("Degrees")] public float armAngle;
+        [Tooltip("Degrees")] public float wristAngle;
+        [Tooltip("Degrees")] public float climbAngle;
+    }
+}
